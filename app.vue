40a546915e1117a19d688538465ac58400cf6fb6
TheUnseenBug <dennis.granheimer@live.se>
2024-07-30 04:24:49 +0200

    installation of api

--- app.vue
+++ app.vue
@@ -1,14 +1,35 @@
 <script setup lang="ts">
-const { data: pokedata } = await useFetch("https://pokeapi.co/api/v2/pokemon");
-const pokemons = pokedata.value.results;
-console.log(pokedata.value.results[0].name);
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { PokemonClient } from "pokenode-ts";
+const pokemonApi = async () => {
+  const api = new PokemonClient();
+
+  const pop = await api.listPokemons(1, 151).then((data) => {
+    return data;
+  });
+  console.log(pop);
+
+  return pop;
+};
+console.log(pokemonApi);
 </script>
 <template>
   <div>
-    <ul id="example-1">
-      <li v-for="item in pokemons" :key="item.name">
-        {{ item.name }}
-      </li>
-    </ul>
+    <Button>Button</Button>
+    <Card v-for="item in pokemons" :key="item.name">
+      <CardHeader>
+        <CardTitle>{{ item.name }}</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent> Card Content </CardContent>
+      <CardFooter> Card Footer </CardFooter>
+    </Card>
   </div>
 </template>
