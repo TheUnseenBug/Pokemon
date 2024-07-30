105c4f1b3568df6f37eba3abc9b39ba720f3fda2
TheUnseenBug <dennis.granheimer@live.se>
2024-07-30 02:43:44 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const { data: pokedata } = await useFetch("https://pokeapi.co/api/v2/pokemon");
+const pokemons = pokedata.value.results;
+console.log(pokedata.value.results[0].name);
+</script>
+<template>
+  <div>
+    <ul id="example-1">
+      <li v-for="item in pokemons" :key="item.name">
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
+</template>
